cc139821052fee6000fafa86ad1104cdd270ec33
jishjish <josh@astrotek.app>
2025-05-25 18:14:16 -0400

    writer thoughts

--- src/transfers/writer.rs
+++ src/transfers/writer.rs
@@ -1,4 +1,6 @@
-// TODO: implement writer function for stacked parquet dataframes
+// TODO: Implement writer function for stacked parquet dataframes.
+//       Current thought is to stack individual columns as unique
+//       dataframes, and then append into one parquet file to write.
 
 
 // example code for writing stacked parquet dfs
@@ -18,5 +20,6 @@
 // let mut combined = df1.vstack(&df2)?;
 
 // // Write to Parquet
+// TODO: write to output file path corresponding to data type (eg: Transfers)
 // let mut file = File::create("data/combined.parquet")?;
 // ParquetWriter::new(&mut file).finish(&mut combined)?;
