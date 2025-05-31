5c0c18996b9061eb3aa80eadd9c3a99fe8ef12ea
jishjish <josh@astrotek.app>
2025-05-31 16:22:25 -0400

    conditional logic based on present columns

--- src/transfers/transfers.rs
+++ src/transfers/transfers.rs
@@ -79,71 +79,65 @@ impl Transfer {
 
         let columns: Vec<String> = schema_check.get_column_names().iter().map(|s| s.to_string()).collect();
 
+
+        // 1) block_number: rle compression
         if columns.contains(&"block_number".to_string()) {
-            // 1) block_number: rle compression
             let mut block_compression: RLECompressedBlockNumberSeries = RLECompressedBlockNumberSeries::new(); 
             let compressed_blocks_df = block_compression.create_compressed_df(&schema_check);
             self.dataframes.push(compressed_blocks_df?);
         }
 
         // 2) transaction_index: rle compression
-        let mut transaction_compression: RLECompressedTransactionIndexSeries = RLECompressedTransactionIndexSeries::new();
-        let compressed_trans_index = transaction_compression.create_compressed_df(&schema_check);
-        self.dataframes.push(compressed_trans_index?);
-
-        // 3) log_index: rle compression
-        let mut log_index_compression = RLECompressedLogIndexSeries::new();
-        let compressed_log_index = log_index_compression.create_compressed_df(&schema_check);
-        self.dataframes.push(compressed_log_index?);
-
-        // 4) transaction_hash: dictionary encoding
-        let mut transaction_hash_compression = DictionaryCompressedTransactionHashSeries::new();
-        let compressed_transaction_df = transaction_hash_compression.create_compressed_df(&schema_check);
-        self.dataframes.push(compressed_transaction_df?);
-
-        // 5) erc20: rle compression
-        let mut token_compression = RLECompressedErc20Series::new();
-        let compressed_tokens_df = token_compression.create_compressed_df(&schema_check);
-        self.dataframes.push(compressed_tokens_df?);
-
-
-        // n) addresses: dictionary encoding
-        let mut address_compression = DictionaryCompressedAddressSeries::new();
-        let compressed_addresses = address_compression.create_compressed_df(&schema_check)?;
-        // println!("Compressed addresses: {:?}", compressed_addresses);
-        for df in compressed_addresses {
-            println!("df memory: {} bytes, shape: {:?}", df.estimated_size(), df.shape());
-            // println!("df is {:?}", df);
-            self.dataframes.push(df);
+        if columns.contains(&"transaction_index".to_string()) {
+            let mut transaction_compression: RLECompressedTransactionIndexSeries = RLECompressedTransactionIndexSeries::new();
+            let compressed_trans_index = transaction_compression.create_compressed_df(&schema_check);
+            self.dataframes.push(compressed_trans_index?);
         }
 
+        // 3) log_index: rle compression
+        if columns.contains(&"log_index".to_string()) {
+            let mut log_index_compression = RLECompressedLogIndexSeries::new();
+            let compressed_log_index = log_index_compression.create_compressed_df(&schema_check);
+            self.dataframes.push(compressed_log_index?);
+        }
 
-//         // n) from_address: dictionary encoding 
-//         let mut from_address_compression = DictionaryCompressedFromAddressSeries::new();
-//         let compressed_from_address_df = from_address_compression.create_compressed_df(&schema_check)?;
-//         // self.dataframes.push(compressed_from_address_df?);
-//         for df in compressed_from_address_df {
-//             self.dataframes.push(df);
-//         }
-// 
-//         // // n) to_address: dictionary encoding 
-//         let mut to_address_compression = DictionaryCompressedToAddressSeries::new();
-//         let compressed_to_address_df = to_address_compression.create_compressed_df(&schema_check)?;
-//         for df in compressed_to_address_df {
-//             self.dataframes.push(df);
-//         }
-//         // self.dataframes.push(compressed_to_address_df?);
+        // 4) transaction_hash: dictionary encoding
+        if columns.contains(&"transaction_hash".to_string()) {
+            let mut transaction_hash_compression = DictionaryCompressedTransactionHashSeries::new();
+            let compressed_transaction_df = transaction_hash_compression.create_compressed_df(&schema_check);
+            self.dataframes.push(compressed_transaction_df?);
+        }
+
+        // 5) erc20: rle compression
+        if columns.contains(&"erc20".to_string()) {
+            let mut token_compression = RLECompressedErc20Series::new();
+            let compressed_tokens_df = token_compression.create_compressed_df(&schema_check);
+            self.dataframes.push(compressed_tokens_df?);
+        }
+
+        // n) addresses (to and/or from): dictionary encoding
+        if columns.contains(&"to_address".to_string()) || columns.contains(&"to_address".to_string()) {
+            let mut address_compression = DictionaryCompressedAddressSeries::new();
+            let compressed_addresses = address_compression.create_compressed_df(&schema_check)?;
+            for df in compressed_addresses {
+                println!("df memory: {} bytes, shape: {:?}", df.estimated_size(), df.shape());
+                self.dataframes.push(df);
+            }
+        }
 
         // 9) value_strings: normalization compression 
-        let mut value_string_compression: NormalizedCompressedValueStrings = NormalizedCompressedValueStrings::new();
-        let comopressed_value_string = value_string_compression.create_compressed_df(&schema_check);
-        self.dataframes.push(comopressed_value_string?);
+        if columns.contains(&"value_strings".to_string()) {
+            let mut value_string_compression: NormalizedCompressedValueStrings = NormalizedCompressedValueStrings::new();
+            let comopressed_value_string = value_string_compression.create_compressed_df(&schema_check);
+            self.dataframes.push(comopressed_value_string?);
+        }
 
         // 11) chain_id: rle compression
-        let mut chain_id_compression = RLECompressedChainIdSeries::new();
-        let compressed_chain_id_df = chain_id_compression.create_compressed_df(&schema_check);
-        // println!("chain id compression: {:?}", compressed_chain_id_df?);
-        self.dataframes.push(compressed_chain_id_df?);
+        if columns.contains(&"chain_id".to_string()) {
+            let mut chain_id_compression = RLECompressedChainIdSeries::new();
+            let compressed_chain_id_df = chain_id_compression.create_compressed_df(&schema_check);
+            self.dataframes.push(compressed_chain_id_df?);
+        }
 
         // write to parquet
         // self.write_parquet(filepath)?;
